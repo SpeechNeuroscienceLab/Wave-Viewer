fa78f8ea4910b39b6b2dc6296e8ac516cbaf7951
hardikkothare <hardikkothare@gmail.com>
2014-12-22 16:02:46 -0800

    Updated moveback.m

--- moveback.m
+++ moveback.m
@@ -17,7 +17,7 @@ for i=1:n_axkids
         break;
     end
 end
-%ih = dsearchn(axkids,h);
+
 if isempty(ih), error('handle(%f) not a child of hax(%f)', h, hax); end
 if ih < n_axkids
   new_axkids(1:(ih-1)) = axkids(1:(ih-1));
